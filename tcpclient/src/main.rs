fb71fa4d7f238f037273b63dbf5b4c73ca70cb45
Alexander Kosarchuk <kvago36@gmail.com>
2024-05-24 06:32:10 +0300

    update client

--- tcpclient/src/main.rs
+++ tcpclient/src/main.rs
@@ -2,48 +2,77 @@ use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
 use tokio::time::{sleep, Duration};
 
+use std::io;
+
 use std::error::Error;
+
+enum State {
+    Init,
+    Hi,
+    Mail,
+    Recipient,
+    Data,
+    Send,
+    End,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    // Connect to a peer
     let mut stream = TcpStream::connect("localhost:4000").await?;
-    // loop {
-    let mut buf = [0; 1024];
+    let mut state = State::Init;
 
-    let n = stream.read(&mut buf).await?;
+    loop {
+        stream.readable().await?;
 
-    if let Ok(data) = String::from_utf8(buf[..n].to_vec()) {
-        println!("{}", data)
+        let mut buf = [0; 4096];
+
+        match stream.try_read(&mut buf) {
+            Ok(0) => break,
+            Ok(n) => {
+                if let Ok(data) = String::from_utf8(buf[..n].to_vec()) {
+                    println!("Message from sever: {}", data)
+                }
+
+                match state {
+                    State::Init => {
+                        state = State::Hi;
+                        stream.write_all(b"HELO sender.example.com").await?;
+                    },
+                    State::Hi => {
+                        state = State::Mail;
+                        stream.write_all(b"MAIL FROM: <sender@example.com>").await?;
+                    },
+                    State::Mail => {
+                        state = State::Recipient;
+                        stream.write_all(b"RCPT TO: <user@yandex.ru>").await?;
+                    },
+                    State::Recipient => {
+                        state = State::Data;
+                        stream.write_all(b"DATA").await?;
+                    },
+                    State::Data => {
+                        state = State::Send;
+                        stream
+                        .write_all(b"To: user@yandex.ru\r\nFrom: sender@example.com\r\nSubject: example of SMTP-message\r\n\r\ntext\r\ntext\r\ntext\r\n.\r\nsd\r\n").await?;
+                    },
+                    State::Send => {
+                        state = State::End;
+                        stream.write_all(b"QUIT").await?;
+                        break;
+                    },
+                    State::End => {
+                        panic!("Closed connection")
+                    }
+                }
+            }
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                continue;
+            }
+            Err(e) => {
+                return Err(e.into());
+            }
+        }
     }
-
-    // Write some data.
-    stream.write_all(b"HELO sender.example.com").await?;
-
-    sleep(Duration::from_millis(100)).await;
-
-    // Write some data.
-    stream.write_all(b"MAIL FROM: <sender@example.com>").await?;
-
-    sleep(Duration::from_millis(100)).await;
-
-    // Write some data.
-    stream.write_all(b"RCPT TO: <user@yandex.ru>").await?;
-
-    sleep(Duration::from_millis(100)).await;
-
-    // Write some data.
-    stream.write_all(b"DATA").await?;
-
-    sleep(Duration::from_millis(100)).await;
-
-    stream
-        .write_all(b"To: user@yandex.ru\r\nFrom: sender@example.com\r\nSubject: example of SMTP-message\r\n\r\ntext\r\ntext\r\ntext\r\n.\r\nsd\r\n").await?;
-
-    sleep(Duration::from_millis(100)).await;
-
-    stream.write_all(b"QUIT").await?;
-    // }
 
     Ok(())
 }
